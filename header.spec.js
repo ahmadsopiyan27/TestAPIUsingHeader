fad2c8d9175a5cc1b2ba01ec9b62b1783caf8e48
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-07-14 20:01:04 +0700

    header

--- header.spec.js
+++ header.spec.js
@@ -4,10 +4,16 @@ describe('coba header', async function() {
     let header = {
         'x-access-token': '321d6a221f8926b5ec41ae89a3b2ae7b'
     }
-    let url = 'http://api.travelpayouts.com';
+    let parameter = {
+        'origin': 'MOW',
+        'destination': 'HKT',
+        'departure_date': '2025-04',
+        'return_date': '2025-05'
+    }
+    let url = 'https://api.travelpayouts.com';
     let endpoint = '/v1/prices/monthly'
 
-    const response = await request(url).get(endpoint).set(header);
-    console.log(response);
+    const response = await request(url).get(endpoint).set(header).query(parameter);
+    console.log(response.body);
     
 })
