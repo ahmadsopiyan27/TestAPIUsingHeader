253a7f41207d97071014d2c495a4e1b9d3537084
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-07-14 19:14:15 +0700

    endpoint

--- header.spec.js
+++ header.spec.js
@@ -1,6 +1,10 @@
+const request = require('supertest')
+
 describe('coba header', async function() {
     let header = {
         'x-access-token': '321d6a221f8926b5ec41ae89a3b2ae7b'
     }
+    let url = 'http://api.travelpayouts.com';
+    let endpoint = '/v1/prices/monthly'
     
 })
