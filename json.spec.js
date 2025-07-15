da172a3ff98a0a35fb26e9548d389f8957d9800a
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-07-15 21:58:42 +0700

    update

--- json.spec.js
+++ json.spec.js
@@ -4,9 +4,23 @@ import json from './data.json' assert { type: 'json' };
 let data = JSON.stringify(json);
 let dataBaru = JSON.parse(data);
 
-console.log('typeof json')
-console.log(typeof json)
-console.log('typeof data')
-console.log(typeof data)
-console.log('typeof dataBaru')
-console.log(typeof dataBaru)
+// console.log('typeof json')
+// console.log(typeof json)
+// console.log('typeof data')
+// console.log(typeof data)
+// console.log('typeof dataBaru')
+// console.log(typeof dataBaru)
+
+// dapat respon dari API dalam bentuk string
+// kita ubah ke object
+// kita handle datanya
+// kita balikin ke string
+// kirim request API
+// let a = json;
+// let a = {...json};
+let a = JSON.parse(JSON.stringify(json));
+a.email = 'emailBaru@gmail.com';
+a.userData.nama = ' aulia'
+
+console.log(a)
+console.log(json)
